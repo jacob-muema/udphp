d8bbfe7163c08be4f4b03d0c24610fcf256803e9
Jacob <jacobmuema02@gmail.com>
2024-09-14 06:54:41 +0300

    Updated datavariables.php with new changes

---
+++ Desktop/PROJECTS/newxamp2/htdocs/udemyphp/datavariables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<>
+    <?php
+  $phrase = "This is the lesson of data types";
+  $age = 40;
+  $gpa = 5.53;
+  $isMale = false;
+  
+    echo "$phrase";
+    echo "$age";
+    echo "$gpa";
+    echo "$isMale";
+
+
+    ?>
+    
+</body>
+</html>
